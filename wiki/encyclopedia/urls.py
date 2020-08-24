6643fca121adbb704c14ffcb185cc15c0795ef4c
TalebZak <z.taleb@aui.ma>
2020-08-24 23:37:23 +0100

    Revert "Finished the entry task(I have to convert it to html now), half done with the search task."
    This reverts commit 88ae6d16cab3c2246a13d56a8a3e4a717d0cfb50.

--- wiki/encyclopedia/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("<str:entry>", views.entry_access, name="entry"),
-    path("<str:search_string>", views.search, name="search")
-]
